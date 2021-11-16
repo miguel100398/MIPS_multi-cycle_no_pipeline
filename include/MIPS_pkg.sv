35eb400f8af0dbc94499854002f3ec87c482fa7a
miguel100398 <miguel_angel_bucio@hotmail.com>
2021-11-16 01:46:40 -0600

    UART, MIPS and memory connected at top

--- include/MIPS_pkg.sv
+++ include/MIPS_pkg.sv
@@ -8,6 +8,7 @@ package MIPS_pkg;
     //////////////////////////Parameters///////////////////////////////////////
     parameter  int unsigned  MIPS_NUM_REGISTERS     = 32;
     parameter  int unsigned  MIPS_DATA_WIDTH        = 32;
+    parameter  int unsigned  MIPS_ADDR_WIDTH        = 32;
     localparam int unsigned  MIPS_REG_ADDR_WIDTH    = $clog2(MIPS_NUM_REGISTERS);
     parameter  int unsigned  MIPS_PC_WIDTH          = 32;
     parameter  int unsigned  MIPS_INSTRUCTION_WIDTH = 32;
@@ -23,6 +24,7 @@ package MIPS_pkg;
     ////////////////////////Typedefs//////////////////////////////////////////
     typedef logic[MIPS_REG_ADDR_WIDTH-1:0]    mips_reg_addr_t;
     typedef logic[MIPS_DATA_WIDTH-1:0]        mips_data_t;
+    typedef logic[MIPS_ADDR_WIDTH-1:0]        mips_addr_t;
     typedef logic[MIPS_PC_WIDTH-1:0]          mips_pc_t;
     typedef logic[MIPS_INSTRUCTION_WIDTH-1:0] mips_instruction_t;
     typedef logic[MIPS_IMMEDIATE_WIDTH-1:0]   mips_immediate_t;
--- src/uart_mips.sv
+++ src/uart_mips.sv
@@ -7,19 +7,75 @@ module uart_mips(
 	 input  logic rst_n
 );
 
+import MIPS_pkg::*;
+import UART_pkg::*;
+import UART_csr_pkg::*;
+
+//MIPS signals
+mips_addr_t mips_addr;
+mips_data_t mips_wr_data;
+logic mips_wr_en;
+mips_data_t mips_rd_data;
+//Memoryu signals
+logic chip_sel_memory;
+logic[MIPS_DATA_WIDTH-2:0] mem_addr;
+mips_data_t mem_rd_data;
+//Uart signals 
+logic chip_sel_uart;
+uart_csr_addr_t uart_csr_wr_addr;
+uart_csr_addr_t uart_csr_rd_addr;
+uart_csr_data_t uart_csr_wr_data;
+uart_csr_data_t uart_csr_rd_data;
+logic           uart_csr_wen;
+logic           uart_csr_ren;
+
+
+//MIPS signals 
+assign mips_rd_data        = (chip_sel_memory) ? mem_rd_data : uart_csr_rd_data;
+//Memory signals
+assign chip_sel_memory     = ~mips_addr[MIPS_ADDR_WIDTH-1];
+assign mem_addr            = mips_addr[MIPS_ADDR_WIDTH-2:0];
+//UART signals
+assign chip_sel_uart       = mips_addr[MIPS_ADDR_WIDTH-1];
+assign uart_csr_wr_addr    = mips_addr[2:0];
+assign uart_csr_rd_addr    = mips_addr[2:0];
+assign uart_csr_wr_data    = mips_wr_data;
+assign uart_csr_wen        = chip_sel_uart && mips_wr_en;
+assign uart_csr_ren        = chip_sel_uart && ~mips_wr_en;
+
+//UART
 UART uart0(
 	.clk(clk),
-	.rst_n(rst_n)
+	.rst_n(rst_n),
+    .csr_wr_addr(uart_csr_wr_addr),
+    .csr_wr_data(uart_csr_wr_data),
+    .csr_wen(uart_csr_wen),
+    .csr_rd_addr(uart_csr_rd_addr),
+    .csr_ren(uart_csr_ren),
+    .csr_rd_data(uart_csr_rd_data)
 );
 
+//MIPS
 MIPS mips0(
 	.clk(clk),
-	.rst_n(rst_n)
+	.rst_n(rst_n),
+    .rd_data_mem(mips_rd_data),
+    .addr_mem(mips_addr),
+    .wr_data_mem(mips_wr_data),
+    .wr_en_mem(mips_wr_en)
 );
 
-//Memories
-ROM instr_mem(
-	.clk(clk)
+instruction_data_memory #(
+    .DATA_WIDTH(MIPS_DATA_WIDTH),
+    .ADDR_WIDTH(MIPS_ADDR_WIDTH-1),
+    .MEMORY_FORMAT("hex")
+)memory(
+    .clk(clk),
+    .chip_sel(chip_sel_memory),
+    .addr(mem_addr),
+    .wr_data(mips_wr_data),
+    .wr_en(mips_wr_en),
+    .rd_data(mem_rd_data)
 );
 
 endmodule: uart_mips
